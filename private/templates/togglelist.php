e59ead8a4a371848a09972f80e6e2d4c3a139372
Tate Deskins <jtd55@case.edu>
2015-05-15 22:48:28 -0400

    checkbox's now toggle, not persistent

---
+++ private/templates/togglelist.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<div id="block-toggle-contetnt" class="block block-toggle-content">
+
+	<h4>Display arXiv subjects:</h4>
+	<div id="toggle-content" class="content">
+		<ul>
+		</ul>
+	</div>
+
+</div>
