99dcc518b4207eef5cf315add740ab8055a89e22
Jim Mertens <jbcm627@gmail.com>
2015-05-19 22:23:18 -0400

    jumbotron: papers voted on between meetings are shown

--- private/templates/jumbotron.php
+++ private/templates/jumbotron.php
@@ -43,26 +43,26 @@ foreach($result as $row){
 		<div class="list-group voted-abstracts">
 			<?php
 			$paper_meeting_times = get_adjacent_meeting_times("end", TRUE /* Get *only* meeting times where papers will be discussed */);
-			$prev_time = date("Y-m-d H:i:s", $paper_meeting_times["prev"]);
-			$next_time = date("Y-m-d H:i:s", $paper_meeting_times["next"]);
+			$prev_time = date("Y-m-d H:i", $paper_meeting_times["prev"]);
+			$next_time = date("Y-m-d H:i", $paper_meeting_times["next"]);
 			$query = "SELECT papers.id, papers.title, papers.authors, papers.abstract, SUM(votes.value) AS value
 								FROM papers
-								JOIN votes ON papers.id=votes.paperid AND votes.date BETWEEN ? AND ?
+								JOIN votes ON papers.id=votes.paperid 
+								WHERE votes.date BETWEEN ? AND ?
 								GROUP BY papers.id ORDER BY value DESC";
 			$result = $coffee_conn->boundQuery($query, array('ss', &$prev_time, &$next_time));
-			$result = array();
 			foreach($result as $paper) {
 				?>
 					<div class="list-group-item voted paper-listing">
 						
 						<h4>
 						<?php 
-							if($paper->value > 0) { ?>
-								<span class='label label-success vote-label' id='article-<?php print $paper->id; ?>-messages-voted'> <?php print "+" . $paper->value; ?> </span>
+							if($paper["value"] > 0) { ?>
+								<span class='label label-success vote-label' id='article-<?php print $paper["id"]; ?>-messages-voted'> <?php print "+" . $paper["value"]; ?> </span>
 							<?php } else { ?>
-								<span class='label label-danger vote-label' id='article-<?php print $paper->id; ?>-messages-voted'> <?php print $paper->value; ?> </span>
+								<span class='label label-danger vote-label' id='article-<?php print $paper["id"]; ?>-messages-voted'> <?php print $paper["value"]; ?> </span>
 							<?php } 
-							$voted_title=format_arxiv_title_voted($paper->title);
+							$voted_title=format_arxiv_title_voted($paper["title"]);
 							if(is_array($voted_title)){ ?>
 								<span class="paper-title-voted"><?php print $voted_title[0]; ?></span> 
 							<?php } else { ?>
@@ -74,11 +74,11 @@ foreach($result as $row){
 							<?php if(is_array($voted_title)){
 								print $voted_title[1] . $voted_title[2];
 							}?>
-							<button type="button" class="btn btn-default btn-xs voted-btn abstract-showhide" data-paperid="<?php print $paper->id?>" id="article-<?php print $paper->id?>-button">Abstract</button>
+							<button type="button" class="btn btn-default btn-xs voted-btn abstract-showhide" data-paperid="<?php print $paper["id"]; ?>" id="article-<?php print $paper["id"]; ?>-button">Abstract</button>
 						</div>
 
 						<span class="user-voters"><?php
-							foreach($paper_votes[$paper->id] as $user_l => $votes_l) {?>
+							foreach($paper_votes[$paper["id"]] as $user_l => $votes_l) {?>
 								<span class="user-sup"> <?php print $user_l;?>
 									<sup><?php
 									if($votes_l>0) {
@@ -92,8 +92,8 @@ foreach($result as $row){
 							}?>
 						</span>
 
-						<div class="voted-paper-abstract" style="display: none;" id="article-<?php print $paper->id?>-abstract">
-							<h5><?php print $paper->authors; ?></h5>
+						<div class="voted-paper-abstract" style="display: none;" id="article-<?php print $paper["id"]; ?>-abstract">
+							<h5><?php print $paper["authors"]; ?></h5>
 							<div class="article-button-holder voted">
 								<button type="button" class="btn btn-xs btn-success btn-upvote" aria-label="Left Align" data-toggle="tooltip" data-placement="bottom" title="Increase Rating">
 									<span class="glyphicon glyphicon-align-left glyphicon-thumbs-up" aria-hidden="true"></span>
@@ -102,21 +102,21 @@ foreach($result as $row){
 									<span class="glyphicon glyphicon-align-left glyphicon-thumbs-down" aria-hidden="true"></span>
 								</button>
 								<?php 
-								if(isset($votes[$paper->id])) { ?>
+								if(isset($votes[$paper["id"]])) { ?>
 									<?php 
-									if($votes[$paper->id] > 0) { ?>
-										<span class='article-messages bg-success' id='article-voted-<?php print $paper->id; ?>-messages'> <?php print "Rated: +" . $votes[$paper->id]; ?> </span>
+									if($votes[$paper["id"]] > 0) { ?>
+										<span class='article-messages bg-success' id='article-voted-<?php print $paper["id"]; ?>-messages'> <?php print "Rated: +" . $votes[$paper["id"]]; ?> </span>
 											<?php 
 									} else { ?>
-										<span class='article-messages bg-danger' id='article-voted-<?php print $paper->id; ?>-messages'> <?php print "Rated: " . $votes[$paper->id]; ?> </span>
+										<span class='article-messages bg-danger' id='article-voted-<?php print $paper["id"]; ?>-messages'> <?php print "Rated: " . $votes[$paper["id"]]; ?> </span>
 											<?php 
 									} 
 								} else { ?>
-									<span class='article-messages' id='article-voted-<?php print $paper->id; ?>-messages'>&nbsp;</span>
+									<span class='article-messages' id='article-voted-<?php print $paper["id"]; ?>-messages'>&nbsp;</span>
 									<?php
 								} ?>
 							</div>
-							<?php print $paper->abstract ?>
+							<?php print $paper["abstract"] ?>
 						</div>
 
 					</div><!-- end #list-group-item -->
@@ -125,14 +125,12 @@ foreach($result as $row){
 		</div><!-- end #list-group -->
 
 		<p>
-			<?php if($params->get('d')) { ?>
-				Showing votes from ...
-			<?php } else { ?>
-				<em>Next meeting is <?php
-					$adjacent_meetings = get_adjacent_meeting_times("start", FALSE, time());
-					print date("l M jS, H:i a", $adjacent_meetings["next"]);
-				?>.</em>
-			<?php } ?>
+			<em>
+				Showing votes from <?php print $prev_time; ?> to <?php print $next_time; ?> |
+				Next meeting is <?php
+				$adjacent_meetings = get_adjacent_meeting_times("start", FALSE, time());
+				print date("l M jS, H:i a", $adjacent_meetings["next"]);
+			?>.</em>
 		</p>
 
 	</div>
