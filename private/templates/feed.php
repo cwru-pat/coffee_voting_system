d4bd08e42a1732f69605848a8eaf59d8887a9ddb
Jim Mertens <jbcm627@gmail.com>
2015-05-26 16:51:43 -0400

    Big changes... provide a way to add/edit/view individual post content. This hasn't been tested, and needs to be linked up.

--- private/templates/feed.php
+++ private/templates/feed.php
@@ -15,36 +15,16 @@ foreach($arxivs as $arxiv) {
   <div class="panel-body arxiv">
     <ul class="feed list-group">
     <?php
-      $query = "SELECT * FROM papers WHERE subject = '$arxiv' AND date BETWEEN '{$date} 00:00:00' AND '{$date} 23:59:59'";
-      $result = $coffee_conn->dbQuery($query);
+      $begin_date = $date . " 00:00:00";
+      $end_date = $date . " 23:59:59";
+      $result = $coffee_conn->boundQuery(
+          "SELECT * FROM papers WHERE subject = ? AND date BETWEEN ? AND ?",
+          array('sss', &$arxiv, &$begin_date, &$end_date)
+        );
       foreach($result as $paper) {
         ?>
         <li class='article list-group-item' id='article-<?php print $paper->id; ?>'>
-          <h4><?php print format_arxiv_title($paper->title); ?></h4>
-          <h5><?php print format_arxiv_authors($paper->authors); ?></h5>
-          <div class="article-button-holder" data-paperid="<?php print $paper->id; ?>">
-          <a role="button" href="#" class="btn btn-xs btn-success btn-upvote" aria-label="Left Align" data-toggle="tooltip" data-placement="bottom" title="Increase Rating">
-            <span class="glyphicon glyphicon-align-left glyphicon-thumbs-up" aria-hidden="true"></span>
-          </a>
-          <a role="button" href="#" class="btn btn-xs btn-danger btn-downvote" aria-label="Left Align" data-toggle="tooltip" data-placement="bottom" title="Decrease Rating">
-            <span class="glyphicon glyphicon-align-left glyphicon-thumbs-down" aria-hidden="true"></span>
-          </a>
-          <?php if(isset($votes[$paper->id])) { ?>
-            <?php if($votes[$paper->id] > 0) { ?>
-              <span class='article-messages bg-success' id='article-<?php print $paper->id; ?>-messages'>
-              <?php print "Rated: +" . $votes[$paper->id]; ?>
-            <?php } else { ?>
-              <span class='article-messages bg-danger' id='article-<?php print $paper->id; ?>-messages'>
-              <?php print "Rated: " . $votes[$paper->id]; ?>
-            <?php } ?>
-            </span>
-          <?php } else { ?>
-            <span class='article-messages ' id='article-<?php print $paper->id; ?>-messages'>&nbsp;</span>
-          <?php } ?>
-          </div>
-          <p>
-            <?php print o($paper->abstract); ?>
-          </p>
+          <?php include "paper.php"; ?>
         </li>
         <?php
       }
