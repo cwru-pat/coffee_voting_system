d15fbc5ebf2201dda551e5da5f9c5bab63c7978c
Jim Mertens <jbcm627@gmail.com>
2015-05-27 17:48:36 -0400

    An ugly but working search feature now exists.

--- index.php
+++ index.php
@@ -11,18 +11,31 @@ require_once("private/templates/jumbotron.php");
 ?>
 <script type="text/javascript" src="<?php print path(); ?>js/sidebar.js"></script>
 <div class="container">
+  
   <div class="row">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <input id="arxiv_search" class="form-control" placeholder="Search...">
+        <div id="arxiv_search_results"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    
     <div class="col-sm-3" id="leftCol">
-      <div class="nav nav-stacked btn-group-vertical toggle-content" id="arxiv-toggle-list" role="group" data-clampedwidth="#leftCol">
+      <div class="btn-group-vertical toggle-content" id="arxiv-toggle-list" role="group" data-clampedwidth="#leftCol">
         <a class="list-group-item active">
           <h5 class="list-group-item-heading"> Display arXiv results from:</h5>
         </a>
         <?php require_once("private/templates/calendar.php"); ?>
       </div>
     </div>
+
     <div class="col-sm-9">
       <?php require_once("private/templates/feed.php"); ?>
     </div>
+
   </div>
 </div>
 
