ba1b6dd5097e6a968770395dbb2ad37bc33e6db4
Jim Mertens <jbcm627@gmail.com>
2015-05-27 20:05:27 -0400

    Enable tooltips

--- private/templates/header.php
+++ private/templates/header.php
@@ -42,6 +42,8 @@
       MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
     </script>
 
+    <script type="text/javascript" src="<?php print path(); ?>js/main.js"></script>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
