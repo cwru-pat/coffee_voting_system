da3ef00e5e57b55ace8b76fe01e75c5abf3bc400
deskinsjt <deskinsjt@users.noreply.github.com>
2015-05-30 20:51:12 -0500

    darn semicolon removed.

--- private/templates/header.php
+++ private/templates/header.php
@@ -41,7 +41,7 @@
     <script type="text/javascript" src="<?php print path(); ?>js/search.js"></script>
     
 
-    <?php if(isset($mathjax)? $mathjax: false;){?>
+    <?php if(isset($mathjax)? $mathjax: false){?>
         <script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
         <script type="text/x-mathjax-config">
           MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
