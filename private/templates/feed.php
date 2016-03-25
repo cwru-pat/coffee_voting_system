4a5445a0efab29911e6f73e842154adf95676237
deskinsjt <jtd55@case.edu>
2016-03-25 18:18:52 -0400

    Moved added code to snarxiv.php which is only included on 04-01

--- private/templates/feed.php
+++ private/templates/feed.php
@@ -17,35 +17,7 @@ $r_params=array();
 
 //////////////  SOME 04-01 TWEAKS  //////////////
 if(date('m-d',time())=='04-01') {
-?>
-<div class="panel panel-info arxiv">
-    <div class="panel-heading toggle arxiv">
-        <h3 class="panel-title">
-            <span class='hidden-sm hidden-xs hidden-md'>Papers from</span>
-            <span class= "cookieTitle">sna-rx.IV</span>
-        </h3>
-    </div>
-    <div class="panel-body arxiv">
-        <ul class="feed list-group">
-        <?php
-            $papers = $coffee_conn->boundQuery(
-                "SELECT * FROM papers WHERE subject = 'sna-rx.IV' AND date BETWEEN ? AND ? ORDER BY id",
-                array('ss', &$begin_date, &$end_date)
-            );
-            if (count($papers) == 0) {
-                print "<li class='list-group-item'>No papers in this section today!</li>";
-            } else {
-                foreach ($papers as $paper) {
-                    print "<li class='article list-group-item' id='article-" . $paper['id'] . "'>";
-                    include "paper.php";
-                    print "</li>";
-                }
-            }
-        ?>
-        </ul>
-    </div>
-</div>
-<?php
+    require_once "private/templates/snarxiv.php";
 }
 ////////////// END OF 04-01 TWEAKS //////////////
 
