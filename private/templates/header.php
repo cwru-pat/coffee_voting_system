7d905507612698f2de7cef601d2a139c2c2bd1ce
deskinsjt <jtd55@case.edu>
2017-06-09 21:57:09 -0400

    Adjusted the MathJax CDN as it is changed

--- private/templates/header.php
+++ private/templates/header.php
@@ -42,7 +42,7 @@
     <?php
     if (isset($mathjax)? $mathjax: false) {
     ?>
-        <script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+    <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
         <script type="text/x-mathjax-config">
             MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
         </script>
