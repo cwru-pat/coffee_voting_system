c1d8d778d4b54606620443abbf23a412df33265c
deskinsjt <jtd55@case.edu>
2018-03-27 16:13:44 -0400

    Fixed so that we append to the log files instead of overwrite.

--- private/coffee_cron.sh
+++ private/coffee_cron.sh
@@ -3,12 +3,12 @@
 
 # To automate this, set up a cron job, similar to
 
-# 0 0,21,22 * * * /PATH_TO_WEB_DIRECTORY/private/coffee_cron.sh > /PATH_TO_WEB_DIRECTORY/private/log/cron.log
+# 0 0,21,22 * * * /PATH_TO_WEB_DIRECTORY/private/coffee_cron.sh >> /PATH_TO_WEB_DIRECTORY/private/log/cron.log
 
 # which will run at midnight, 9 pm, and 10 pm daily.
 
 my_dir=$(dirname -- "$0")
 cd "$my_dir"
 touch log/error.log log/import.log log/cron.log log/logrotate.log
-php cronjob.php > log/import.log
+php cronjob.php >> log/import.log
 logrotate -s log/logrotate.log logrotate.conf
